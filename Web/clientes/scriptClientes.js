5b6453931a266753841607506b8f580d2e379119
alejandraparada <carla.parada.aguilera@gmail.com>
2023-05-18 13:38:06 +0200

    Modificar campos cliente

--- Web/clientes/scriptClientes.js
+++ Web/clientes/scriptClientes.js
@@ -4,10 +4,11 @@ require([
     "dojo/parser",
     "dojo/ready",
     "dojo/on",
+    "esri/dijit/AttributeInspector",
     "dijit/layout/ContentPane",
     "dijit/layout/BorderContainer"
   ], function (
-    FeatureLayer, FeatureTable, parser, ready, on, ContentPane, BorderContainer
+    FeatureLayer, FeatureTable, parser, ready, on, AttributeInspector, ContentPane, BorderContainer
   ) {
 
     parser.parse();
@@ -37,7 +38,7 @@ require([
         myFeatureTable.startup();
 
         myFeatureTable.on("load", function(evt){
-          
+          //Dar de alta nuevo cliente - añadir cliente
           let button = document.getElementById("addCustomerButton");
           button.addEventListener("click", (evt) => {
 
@@ -45,7 +46,7 @@ require([
               attributes: {
               }
             };
-              
+            //Obtener Elementos input del formulario por el nombre de la clase  
             let inputs = document.getElementsByClassName("form_field");
             for(let i = 0; i < inputs.length; i++){
               console.log("inputsLength",inputs.length)
@@ -57,21 +58,62 @@ require([
                 adds.attributes[inputs[i].alt] = inputs[i].value;
               }
             }
-
             myFeatureTable.featureLayer.applyEdits([adds], null, null);
             myFeatureTable.refresh();
           });
 
+          //Crear campos segun FeatureTable 
+          myFeatureTable.featureLayer.fields.forEach(field => {
+
+            var input = document.createElement("input");
+            input.setAttribute("class", "form_field");
+            input.setAttribute("id", field.name);
+            input.setAttribute("placeholder", field.name);
+            input.setAttribute("alt", field.name);
+            if(field.name.toUpperCase() == "OBJECTID"){
+              input.setAttribute("disabled", true);
+              input.setAttribute("readonly", true)
+            }
+            if(field.name.includes("Password")){
+              input.setAttribute("type", "password");
+            }
+            else{
+              input.setAttribute("type", "text");
+            }
+            //Valores por defecto
+            if(field.name.includes("Nombre")){
+              input.setAttribute("value", `Carla`);
+            }
+            if(field.name.includes("URL")){
+              input.setAttribute("value", `https://carla`);
+            }
+            if(field.name.includes("Usuario")){
+              input.setAttribute("value", `caparada`);
+            }
+            if(field.name.includes("Password")){
+              input.setAttribute("value", `carlapa123`);
+            }
+            if(field.name.includes("Codigo")){
+              input.setAttribute("value", `28805`);
+            }
+            if(field.name.includes("Provincia")){
+              input.setAttribute("value", `Madrid`);
+            }
+            button.parentNode.insertBefore(input, button);
+            
+          });
+
+          //Modificar cliente existente
           let buttonUpdate = document.getElementById("updateCustomerButton");
           buttonUpdate.addEventListener("click", (evt) => {
 
             let updates = {
-              attributes: {
-              }
+              attributes: {},
+              geometry: null
             };
               
             let inputs = document.getElementsByClassName("form_field");
-            for(let i = 1; i < inputs.length; i++){
+            for(let i = 0; i < inputs.length; i++){
               if(inputs[i].type != "submit"){
                 if(inputs[i].value == null || inputs[i].value === ""){
                     alert("Es necesario que rellenes todos los campos");
@@ -81,69 +123,27 @@ require([
               }
             }
 
-            myFeatureTable.featureLayer.applyEdits(null,[updates], null);
-            myFeatureTable.refresh();
-          }); 
-          
-          myFeatureTable.featureLayer.fields.forEach(field => {
-            if(field.name.toUpperCase() != "OBJECTID")
-            {
-              var input = document.createElement("input");
-              input.setAttribute("class", "form_field");
-              input.setAttribute("id", field.name);
-              input.setAttribute("placeholder", field.name);
-              input.setAttribute("alt", field.name);
-              if(field.name.includes("Password")){
-                input.setAttribute("type", "password");
-              }
-              else{
-                input.setAttribute("type", "text");
-              }
-              // if(field.name.includes("Nombre")){
-              //   input.setAttribute("value", `Carla`);
-              // }
-              // if(field.name.includes("URL")){
-              //   input.setAttribute("value", `https://carla`);
-              // }
-              // if(field.name.includes("Usuario")){
-              //   input.setAttribute("value", `caparada`);
-              // }
-              // if(field.name.includes("Password")){
-              //   input.setAttribute("value", `carlapa123`);
-              // }
-              // if(field.name.includes("Codigo")){
-              //   input.setAttribute("value", `28805`);
-              // }
-              // if(field.name.includes("Provincia")){
-              //   input.setAttribute("value", `Madrid`);
-              // }
-              button.parentNode.insertBefore(input, button);
-            }
-          });
-        });
+            myFeatureTable.featureLayer.applyEdits(null, [updates], null, function(addResults, updateResults, deleteResults) {
+              console.log("Edición aplicada correctamente");
+            }, function(error) {
+              console.error("Error al aplicar la edición: ", error);
+            });
 
+            // myFeatureLayer.applyEdits(null,[updates], null);
+            myFeatureTable.refresh();
+          });
+
+        });
+        
+        //Seleccionar cliente y mostrar en formulario
         myFeatureTable.on("row-select", function(evt){
-          console.log("select", evt);
           myFeatureTable.featureLayer.fields.forEach(field => {
-            console.log("a",evt.rows[0].data)
             var data = evt.rows[0].data
-            // for (opt in data){
-              // console.log("dataopt",data[opt])
-              var formField = document.getElementById(field.name)
-              console.log(field.name,formField)
-              for(let i = 1; i < Object.keys(data).length; i++){
-                console.log("l",Object.keys(data)[i])
-                if(Object.keys(data)[i] == field.name ){
-                  console.log("valor",Object.keys(data)[i])
-                  console.log("valor2",data[i])
-                  // document.getElementById(field.name).value = data[i]
-                }
-                // if(formField.namedItem == field.name){
-                //   console.log("name2",document.getElementsByClassName("form_field").field.name)
-                // }
-                // document.getElementsByClassName("form_field")[i].value = data[opt]
+            for(let i = 0; i < Object.keys(data).length; i++){
+              if(Object.keys(data)[i] == field.name ){
+                document.getElementById(field.name).value = Object.values(data)[i]
               }
-            // }
+            }
           });
         }); 
         
